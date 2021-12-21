a42949f4ba025d4eae64a9c2d942b051d82a29a8
tedhasserud <ted.hasserud@hotmail.se>
2021-12-21 14:57:35 +0100

    Add project files.

---
+++ Code Rangers Ehandel/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Code_Rangers_Ehandel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void hej()
+        {
+            string hej = "hej";
+        }
+    }
+}
