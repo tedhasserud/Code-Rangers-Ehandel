c59be07dac9703ec5d45ef0f307e1dd1f89ac6c5
Alexander Fredlund <79082170+afaxalex@users.noreply.github.com>
2021-12-28 14:27:29 +0100

    push 1

---
+++ Code Rangers Ehandel/Bicycles/BicycleForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Code_Rangers_Ehandel.Bicycles
+{
+    public partial class BicycleForm : Form
+    {
+        public BicycleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            Register register = new Register();
+            register.Show();
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BicycleForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
